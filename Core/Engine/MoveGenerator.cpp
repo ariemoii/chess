e2dbd23410f7fc1e9308340d5b9e4ace57aa26ed
ariemoii <arendspijker@gmail.com>
2025-05-21 16:01:09 +0200

    added playervplayer class,

--- Core/Engine/MoveGenerator.cpp
+++ Core/Engine/MoveGenerator.cpp
@@ -11,7 +11,11 @@ std::vector<Move> MoveGenerator::generateLegalMoves(Board* board) {
   std::vector<Move> pseudoLegalMoves = pseudoLegalMoveGenerator.generatePseudolegalMoves(board);
   std::vector<Move> fullyLegalMoves;
   for(Move moveToCheck : pseudoLegalMoves) {
+    Piece::Team ourSide = board->sideToMove;
     moveMaker.makeMove(moveToCheck, board);
+    if(isInCheck(board, ourSide)) {
+      std::cout << "AHAHAH THAT RESULTS IN CHECK MORON\n";
+    }
     std::vector<Move> pseudoLegalMovesOpp = pseudoLegalMoveGenerator.generatePseudolegalMoves(board);
     bool illegal = false;
     for(Move oppMove : pseudoLegalMovesOpp) {
@@ -39,8 +43,18 @@ bool MoveGenerator::isInCheck(Board* board, Piece::Team team) {
   for(int i = 0; i < board->theBoard.size(); i++) {
     int piece = board->theBoard[i];
     if(piece == 0) {
+      //there is no piece
       continue;
     }
-    
+    if(Piece::isTeam(piece, team)) {
+      //we cannot attack our own king
+      continue;
+    }
+    int kingLoc = (team == Piece::WHITE) ? board->whiteKingLoc : board->blackKingLoc;
+    if(moveData.canAttack(piece, i, kingLoc, board)) {
+      //they can attack the king, so we are in check.
+      return true;
+    }
   }
+  return false;
 }
